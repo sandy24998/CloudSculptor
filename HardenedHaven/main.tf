303c62602f37379e546e82bef3c5195a608466c5
Sandeep Pandit <54panditsandeep@gmail.com>
2025-12-04 17:09:15 +0000

    vpc public subnet resource created using terraform

--- HardenedHaven/main.tf
+++ HardenedHaven/main.tf
@@ -4,3 +4,16 @@ resource "aws_vpc" "main-vpc" {
       Name = "${var.environment}-vpc"
 }
 }
+
+resource "aws_subnet" "public-subnet" {
+  count = length(var.public_subnets)
+  vpc_id = aws_vpc.main-vpc.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-${var.availability_zones[count.index]}"
+    Type = "Public"
+  }
+}
