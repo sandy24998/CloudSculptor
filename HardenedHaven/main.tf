4f024b32751f8936e50b12145e70e36f25e56fe0
Sandeep Pandit <54panditsandeep@gmail.com>
2025-12-04 17:36:47 +0000

    vpc public route table resource created using terraform

--- HardenedHaven/main.tf
+++ HardenedHaven/main.tf
@@ -62,3 +62,14 @@ resource "aws_nat_gateway" "nat" {
 
   depends_on = [aws_internet_gateway.igw]
 }
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main-vpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.id
+  }
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
